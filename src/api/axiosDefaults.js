9af76aa501e17666003b3e7a43d1a141b535c8d7
Alexander Elbert <alexander91elbert@gmail.com>
2024-02-29 12:56:08 +0100

    Add default baseURL and headers in axios configuration.

---
+++ src/api/axiosDefaults.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+axios.defaults.baseURL = 'https://catch-chronicle-api-d205d9d4b14c.herokuapp.com';
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
