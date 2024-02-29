77b57665d0c9ff017a6b51dcf8a00b0165e71f21
Alexander Elbert <alexander91elbert@gmail.com>
2024-02-29 14:32:22 +0100

    Add component for creating new catch entry form with image upload handling.

---
+++ src/pages/catches/CatchCreateForm.js
@@ -0,0 +1,268 @@
+import React, { useState } from "react";
+
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+
+import Upload from "../../assets/upload.png";
+
+import styles from "../../styles/CatchCreateEditForm.module.css";
+import appStyles from "../../App.module.css";
+import btnStyles from "../../styles/Button.module.css";
+import Asset from "../../components/Asset";
+import { Image } from "react-bootstrap";
+
+function CatchCreateForm() {
+
+  const [errors, setErrors] = useState({});
+
+  const [postData, setPostData] = useState({
+    caption: "",
+    species: "",
+    method: "",
+    weight: "",
+    length: "",
+    location: "",
+    latitude: null,
+    longitude: null,
+    time: "",
+    weather: "",
+    lure: "",
+    image: "",
+  });
+  
+  const {
+    caption,
+    species,
+    method,
+    weight,
+    length,
+    location,
+    latitude,
+    longitude,
+    time,
+    weather,
+    lure,
+    image,
+  } = postData;
+
+  const handleChange = (event) => {
+    setPostData({
+      ...postData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const handleChangeImage = (event) => {
+    if (event.target.files.length) {
+      URL.revokeObjectURL(image);
+      setPostData({
+        ...postData,
+        image: URL.createObjectURL(event.target.files[0]),
+      });
+    }
+  };
+
+
+  const textFields = (
+    <div className="text-center">
+      {/* Caption */}
+        <Form.Group>
+        <Form.Label>Caption</Form.Label>
+        <Form.Control
+            type="text"
+            name="caption"
+            value={caption}
+            onChange={handleChange}
+        />
+        </Form.Group>
+
+        {/* Species */}
+        <Form.Group>
+        <Form.Label>Species</Form.Label>
+        <Form.Control
+            type="text"
+            name="species"
+            value={species}
+            onChange={handleChange}
+        />
+        </Form.Group>
+
+        {/* Method */}
+        <Form.Group>
+        <Form.Label>Method</Form.Label>
+        <Form.Control
+            as="select"
+            name="method"
+            value={method}
+            onChange={handleChange}
+        >
+            <option value="">Select method</option>
+            <option value="flyrod">Fly Rod</option>
+            <option value="spinning">Spinning</option>
+            <option value="trolling">Trolling</option>
+        </Form.Control>
+        </Form.Group>
+
+        {/* Weight */}
+        <Form.Group>
+        <Form.Label>Weight</Form.Label>
+        <Form.Control
+            type="number"
+            name="weight"
+            value={weight}
+            onChange={handleChange}
+        />
+        </Form.Group>
+
+        {/* Length */}
+        <Form.Group>
+        <Form.Label>Length</Form.Label>
+        <Form.Control
+            type="number"
+            name="length"
+            value={length}
+            onChange={handleChange}
+        />
+        </Form.Group>
+
+        {/* Location */}
+        <Form.Group>
+        <Form.Label>Location</Form.Label>
+        <Form.Control
+            type="text"
+            name="location"
+            value={location}
+            onChange={handleChange}
+        />
+        </Form.Group>
+
+        {/* Latitude */}
+        <Form.Group>
+        <Form.Label>Latitude</Form.Label>
+        <Form.Control
+            type="number"
+            name="latitude"
+            value={latitude}
+            onChange={handleChange}
+        />
+        </Form.Group>
+
+        {/* Longitude */}
+        <Form.Group>
+        <Form.Label>Longitude</Form.Label>
+        <Form.Control
+            type="number"
+            name="longitude"
+            value={longitude}
+            onChange={handleChange}
+        />
+        </Form.Group>
+
+        {/* Time */}
+        <Form.Group>
+        <Form.Label>Time</Form.Label>
+        <Form.Control
+            type="time"
+            name="time"
+            value={time}
+            onChange={handleChange}
+        />
+        </Form.Group>
+
+        {/* Weather */}
+        <Form.Group>
+        <Form.Label>Weather</Form.Label>
+        <Form.Control
+            as="select"
+            name="weather"
+            value={weather}
+            onChange={handleChange}
+        >
+            <option value="">Select weather</option>
+            <option value="sunny">Sunny</option>
+            <option value="cloudy">Cloudy</option>
+            <option value="rainy">Rainy</option>
+            <option value="stormy">Stormy</option>
+        </Form.Control>
+        </Form.Group>
+
+        {/* Lure */}
+        <Form.Group>
+        <Form.Label>Lure</Form.Label>
+        <Form.Control
+            type="text"
+            name="lure"
+            value={lure}
+            onChange={handleChange}
+        />
+        </Form.Group>
+
+    
+    
+      <Button
+        className={`${btnStyles.Button} ${btnStyles.Blue}`}
+        onClick={() => {}}
+      >
+        cancel
+      </Button>
+      <Button className={`${btnStyles.Button} ${btnStyles.Blue}`} type="submit">
+        create
+      </Button>
+    </div>
+  );
+
+  return (
+    <Form>
+      <Row>
+        <Col className="py-2 p-0 p-md-2" md={7} lg={8}>
+          <Container
+            className={`${appStyles.Content} ${styles.Container} d-flex flex-column justify-content-center`}
+          >
+            <Form.Group className="text-center">
+              {image ? (
+                <>
+                  <figure>
+                    <Image className={appStyles.Image} src={image} rounded />
+                  </figure>
+                  <div>
+                    <Form.Label
+                      className={`${btnStyles.Button} ${btnStyles.Blue} btn`}
+                      htmlFor="image-upload"
+                    >
+                      Change the image
+                    </Form.Label>
+                  </div>
+                </>
+              ) : (
+                <Form.Label
+                  className="d-flex justify-content-center"
+                  htmlFor="image-upload"
+                >
+                  <Asset
+                    src={Upload}
+                    message="Click or tap to upload an image"
+                  />
+                </Form.Label>
+              )}
+
+              <Form.File
+                id="image-upload"
+                accept="image/*"
+                onChange={handleChangeImage}
+              />
+            </Form.Group>
+            <div className="d-md-none">{textFields}</div>
+          </Container>
+        </Col>
+        <Col md={5} lg={4} className="d-none d-md-block p-0 p-md-2">
+          <Container className={appStyles.Content}>{textFields}</Container>
+        </Col>
+      </Row>
+    </Form>
+  );
+}
+
+export default CatchCreateForm;
