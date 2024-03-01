bc7097fc14596aef5a3c4fcca84290f06f9d1ce4
Alexander Elbert <alexander91elbert@gmail.com>
2024-03-01 10:47:09 +0100

    Add routing in App component to include new pages and dynamic filters.

--- src/App.js
+++ src/App.js
@@ -6,9 +6,15 @@ import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import CatchCreateForm from "./pages/catches/CatchCreateForm";
+import CatchPage from "./pages/catches/CatchPage";
+import CatchesPage from "./pages/catches/CatchesPage";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
+import CatchEditForm from "./pages/catches/CatchEditForm";
 
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
   
   return (
     
@@ -16,10 +22,38 @@ function App() {
       <NavBar />
       <Container className={styles.Main}>
         <Switch>
-          <Route exact path="/" render={() => <h1>Home</h1>} />
+          <Route 
+            exact 
+            path="/" 
+            render={() => (
+              <CatchesPage message="No results found. Adjust your search." />
+            )} 
+          />
+          <Route 
+            exact 
+            path="/feed" 
+            render={() => (
+              <CatchesPage 
+                message="No results found. Adjust your search or follow a user." 
+                filter={`owner__followed__owner__profile=${profile_id}&`} 
+              />
+            )} 
+          />
+          <Route 
+            exact 
+            path="/liked" 
+            render={() => (
+              <CatchesPage 
+                message="No results found. Adjust your search or like a catch." 
+                filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`} 
+              />
+            )} 
+          />
           <Route exact path="/signin" render={() => <SignInForm />} />
           <Route exact path="/signup" render={() => <SignUpForm />} />
           <Route exact path="/catches/create" render={() => <CatchCreateForm />} />
+          <Route exact path="/catches/:id" render={() => <CatchPage />} />
+          <Route exact path="/catches/:id/edit" render={() => <CatchEditForm />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
