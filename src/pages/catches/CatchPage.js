2b29c9379fe437349294ca4688605654c8284b46
Alexander Elbert <alexander91elbert@gmail.com>
2024-03-01 12:54:37 +0100

    Add CommentCreateForm and conditional rendering for comments.

--- src/pages/catches/CatchPage.js
+++ src/pages/catches/CatchPage.js
@@ -8,10 +8,15 @@ import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Catch from "./Catch";
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 function CatchPage() {
   const { id } = useParams();
   const [catches, setCatches] = useState({ results: [] });
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
 
   useEffect(() => {
     const handleMount = async () => {
@@ -34,7 +39,19 @@ function CatchPage() {
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles view for mobile</p>
         <Catch {...catches.results[0]} setCatches={setCatches} catchPage />
-        <Container className={appStyles.Content}>Comments</Container>
+        <Container className={appStyles.Content}>
+        {currentUser ? (
+        <CommentCreateForm
+          profile_id={currentUser.profile_id}
+          profileImage={profile_image}
+          id={id}
+          setCatches={setCatches}
+          setComments={setComments}
+        />
+        ) : comments.results.length ? (
+          "Comments"
+        ) : null}
+        </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
         Popular profiles view for desktop
