f5a59fbdeae44a87e63378a70ffc5bdfffbbebee
Alexander Elbert <alexander91elbert@gmail.com>
2024-03-02 12:00:27 +0100

    Add PopularProfiles component into CatchesPage with mobile and desktop view.

--- src/pages/catches/CatchesPage.js
+++ src/pages/catches/CatchesPage.js
@@ -16,6 +16,7 @@ import { axiosReq } from "../../api/axiosDefaults";
 import NoResults from "../../assets/no-results.png";
 import InfiniteScroll from "react-infinite-scroll-component";
 import { fetchMoreData } from "../../utils/utils";
+import PopularProfiles from "../profiles/PopularProfiles";
 
 function CatchesPage({message, filter = ""}) {
     const [catches, setCatches] = useState({ results: [] });
@@ -48,7 +49,7 @@ function CatchesPage({message, filter = ""}) {
       return (
         <Row className="h-100">
           <Col className="py-2 p-0 p-lg-2" lg={8}>
-            <p>Popular profiles mobile</p>
+            <PopularProfiles mobile />
             <i className={`fas fa-search ${styles.SearchIcon}`} />
             <Form
             className={styles.SearchBar}
@@ -87,7 +88,7 @@ function CatchesPage({message, filter = ""}) {
             )}
           </Col>
           <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
-            <p>Popular profiles for desktop</p>
+            <PopularProfiles />
           </Col>
         </Row>
       );
