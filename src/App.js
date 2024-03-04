84091cbdfedbf9212b1a057cd1c99f7e06187e55
Alexander Elbert <alexander91elbert@gmail.com>
2024-03-04 22:30:02 +0100

    Add page not found component.

--- src/App.js
+++ src/App.js
@@ -15,6 +15,7 @@ import UsernameForm from "./pages/profiles/UsernameForm";
 import UserPasswordForm from "./pages/profiles/UserPasswordForm";
 import ProfileEditForm from "./pages/profiles/ProfileEditForm";
 import MyCatchesPage from "./pages/catches/MyCatchesPage";
+import NotFound from "./components/NotFound";
 
 
 
@@ -77,7 +78,7 @@ function App() {
             path="/profiles/:id/edit"
             render={() => <ProfileEditForm />}
           />
-          <Route render={() => <p>Page not found!</p>} />
+          <Route render={() => <NotFound />} />
         </Switch>
       </Container>
     </div>
