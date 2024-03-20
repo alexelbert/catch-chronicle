a5da00d715c793e943c2058410d2b2615fdb5e92
Alexander Elbert <alexander91elbert@gmail.com>
2024-03-20 16:18:17 +0100

    Fixed responsiveness error for table displaying my catches.

--- src/pages/catches/MyCatchesPage.js
+++ src/pages/catches/MyCatchesPage.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import { axiosReq } from "../../api/axiosDefaults";
 import { Table } from "react-bootstrap";
+import styles from "../../styles/MyCatchesPage.module.css";
 
 function MyCatchesPage() {
   const [myCatches, setMyCatches] = useState([]);
@@ -10,19 +11,21 @@ function MyCatchesPage() {
   useEffect(() => {
     const fetchMyCatches = async () => {
       try {
-        const response = await axiosReq.get(`/catches/?owner__profile=${currentUser.profile_id}`);
-        setMyCatches(response.data.results);
+        if(currentUser && currentUser.profile_id) { // Checking if currentUser and profile_id are not null or undefined
+          const response = await axiosReq.get(`/catches/?owner__profile=${currentUser.profile_id}`);
+          setMyCatches(response.data.results);
+        }
       } catch (error) {
         console.error("Error fetching catches:", error);
       }
     };
     fetchMyCatches();
-  }, [currentUser.profile_id]);
+  }, [currentUser]);
   
   return (
     <div>
       <h1>My Catches</h1>
-      <Table striped bordered hover>
+      <Table striped bordered hover className={styles.table}>
         <thead>
           <tr>
             <th>#</th>
@@ -39,18 +42,18 @@ function MyCatchesPage() {
         </thead>
         <tbody>
           {myCatches.map((catchItem, index) => (
-            <tr key={catchItem.id}>
-              <td>{index + 1}</td>
-              <td>{catchItem.created_at}</td>
-              <td>{catchItem.location}</td>
-              <td>{catchItem.species}</td>
-              <td>{catchItem.method}</td>
-              <td>{catchItem.weight}</td>
-              <td>{catchItem.length}</td>
-              <td>{catchItem.time}</td>
-              <td>{catchItem.weather}</td>
-              <td>{catchItem.lure}</td>
-            </tr>
+          <tr key={catchItem.id}>
+            <td data-label="#">{index + 1}</td>
+            <td data-label="Date">{catchItem.created_at}</td>
+            <td data-label="Location">{catchItem.location}</td>
+            <td data-label="Species">{catchItem.species}</td>
+            <td data-label="Method">{catchItem.method}</td>
+            <td data-label="Weight">{catchItem.weight}</td>
+            <td data-label="Length">{catchItem.length}</td>
+            <td data-label="Time">{catchItem.time}</td>
+            <td data-label="Weather">{catchItem.weather}</td>
+            <td data-label="Lure">{catchItem.lure}</td>
+          </tr>
           ))}
         </tbody>
       </Table>
