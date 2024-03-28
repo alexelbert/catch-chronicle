8daf0f68d37b04750d1122fdd8b7d58ebb79ebf7
Alexander Elbert <alexander91elbert@gmail.com>
2024-03-28 14:09:57 +0100

    Change styles for follow unfollow buttons.

--- src/components/FollowButton.js
+++ src/components/FollowButton.js
@@ -1,26 +1,25 @@
 // FollowButton.js
 import React from 'react';
-import { Button } from "react-bootstrap";
-import btnStyles from "../styles/Button.module.css";
+import styles from "../styles/Button.module.css";
 
 const FollowButton = ({ isFollowing, handleFollow, handleUnfollow, profile }) => {
   if (isFollowing) {
     return (
-      <Button
-        className={`${btnStyles.Button} ${btnStyles.BlackOutline}`}
+      <button
+        className={`${styles.Button}`}
         onClick={() => handleUnfollow(profile)}
       >
         Unfollow
-      </Button>
+      </button>
     );
   } else {
     return (
-      <Button
-        className={`${btnStyles.Button} ${btnStyles.Black}`}
+      <button
+        className={`${styles.Button}`}
         onClick={() => handleFollow(profile)}
       >
         Follow
-      </Button>
+      </button>
     );
   }
 };
