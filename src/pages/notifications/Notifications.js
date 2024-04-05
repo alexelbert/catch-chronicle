d06b6d3539a181e4e75bd266b1efd2e8a1cbbc03
Alexander Elbert <alexander91elbert@gmail.com>
2024-04-05 19:28:51 +0200

    Created new page to display the notifications response.

---
+++ src/pages/notifications/Notifications.js
@@ -0,0 +1,14 @@
+// App.js or another parent component
+
+import React from 'react';
+import NotificationsComponent from '../../components/NotificationsComponent';
+
+function App() {
+  return (
+    <div className="App">
+      <NotificationsComponent />
+    </div>
+  );
+}
+
+export default App;
