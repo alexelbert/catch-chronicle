05a45f61a7e64e81bfdbf768f138f34055628416
Alexander Elbert <alexander91elbert@gmail.com>
2024-04-07 14:05:36 +0200

    Add notifications count with styles.

--- src/components/NotificationsComponent.js
+++ src/components/NotificationsComponent.js
@@ -5,7 +5,6 @@ import styles from '../styles/NotificationsComponent.module.css';
 import { axiosReq } from '../api/axiosDefaults';
 import Asset from './Asset';
 
-
 const NotificationsComponent = () => {
   const [notifications, setNotifications] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
@@ -34,20 +33,27 @@ const NotificationsComponent = () => {
         prevNotifications.filter(notification => notification.id !== id)
       );
     } catch (error) {
-      console.error('Failed to delete the notification:', error);
+      console.error('Failed to remove the notification:', error);
     }
   };
-  
 
-  const BellIcon = () => (
-    <button onClick={() => setModalShow(true)} className={styles.bellIcon}>
-      <i className="fa-regular fa-bell"></i>
-    </button>
+  // Calculating the count of new (unread) notifications
+  const newNotificationsCount = notifications.filter(n => !n.is_read).length;
+
+  const BellIcon = ({ newNotificationsCount }) => (
+    <div className={styles.bellIconWrapper}>
+      <button onClick={() => setModalShow(true)} className={styles.bellIcon}>
+        <i className="fa-regular fa-bell"></i>
+      </button>
+      {newNotificationsCount > 0 && (
+        <span className={styles.notificationDot}>{newNotificationsCount}</span>
+      )}
+    </div>
   );
 
   return (
     <>
-      <BellIcon />
+      <BellIcon newNotificationsCount={newNotificationsCount} />
       <Modal
         show={modalShow}
         onHide={() => setModalShow(false)}
@@ -62,18 +68,18 @@ const NotificationsComponent = () => {
         </Modal.Header>
         <Modal.Body>
           {isLoading ? (
-            < Asset />
+            <Asset />
           ) : notifications.length ? (
             notifications.map((notification) => (
               <div key={notification.id} className={styles.notificationItem}>
-                {notification.notification_text} - {new Date(notification.created_at).toLocaleDateString()}
+                {notification.notification_text}  {notification.created_at}
                 <span onClick={() => removeNotification(notification.id)} className={styles.removeButton}>
-                X
+                  X
                 </span>
               </div>
             ))
           ) : (
-            <p>No notifications</p>
+            <p>You have no new notifications</p>
           )}
         </Modal.Body>
         <Modal.Footer>
